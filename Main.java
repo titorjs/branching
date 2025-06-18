c95af213c7f5aa09dd7667d3577b09131c533987
Tito Jaramillo S. <titoj740@gmail.com>
2025-06-17 20:47:12 -0500

    version certificada

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
     public static void main(String [] args){
         Saludo saludo = new Saludo();
         System.out.println(saludo.generarSaludo("Mundo"));
+        System.out.println("versión certificada v1.0.0");
     }
 }
